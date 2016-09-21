5b2b592e7bad35517f596a7a88005ad4a6f2f7f1
Pranav Gupta <pranavgupta8@hotmail.com>
2016-09-21 20:53:28 +0530

    complete app; bugs fixed

--- js/app.js
+++ js/app.js
@@ -12,6 +12,60 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	$(".new").click(function(){
+  		newGame();
+  	});
+
+	randomNumberGenerator();
+
+	$("#guessButton").click(function(){
+
+		i += 1;
+		$("span#count").text(i);
+
+		guessNumber = $("#userGuess").val();
+		
+		$('ul#guessList').append('<li>' + guessNumber + '</li>');
+
+		hotOrCold();
+
+	});  	
+
 });
 
+var randomNumber;
+var guessNumber;
+var i = 0;
 
+function newGame() {
+
+	$('h2#feedback').text('Make your Guess!');
+	$('span#count').text('0');
+	$('ul#guessList').text('');
+	$("#userGuess").val("");
+	
+	i = 0;
+	
+	randomNumberGenerator();
+}
+
+function randomNumberGenerator() {
+
+	randomNumber = Math.floor(Math.random() * 100);
+}
+
+function hotOrCold() {
+
+	var difference = Math.abs(randomNumber - guessNumber);
+
+	if(difference > 50) $('h2#feedback').text("Ice Cold!");
+	else if(difference<=50 && difference>30) $('h2#feedback').text("Cold!");
+	else if(difference<=30 && difference>20) $('h2#feedback').text("Warm!");
+	else if(difference<=20 && difference>10) $('h2#feedback').text("Hot!");
+	else if(difference<=10 && difference>=1) $('h2#feedback').text("Very Hot!");
+	else if(difference == 0) { 
+		$('h2#feedback').text("Right Guess!");
+		alert("Smart Pants! Yoouuuuuu Guessed It!");
+		newGame();
+	}
+}	
